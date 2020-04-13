4be8b941911f5d0b0401a70b4031ef7066b3e4a5
shashankrk46 <shashankrk46@gmail.com>
2020-04-13 14:03:13 +0530

    added marker info window

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ window.onload=()=>{
   displayStores();
 
 }
+var map;
+      var markers = [];
+      var infoWindow;
 
 
 
@@ -12,6 +15,8 @@ function initMap() {
       center: losAngeles,
       zoom: 11,
       });
+      infoWindow = new google.maps.InfoWindow();
+      showStoresMarker();
 //       var marker = new google.maps.Marker({
 //         position: myLatLng,
 //         map: map,
@@ -37,7 +42,7 @@ function initMap() {
   function displayStores(){
     let storesHtml=' ';
     stores.forEach((store,index)=>{
-      console.log(store,index)
+      // console.log(store,index)
       
       // var address=store.addressLines;
 
@@ -55,7 +60,7 @@ function initMap() {
      </div>
    </div>
      <div class="store-number-container">
-       <div class="store-number">1</div>
+       <div class="store-number">${index+1}</div>
      </div>
      </div>`
     })
@@ -63,31 +68,44 @@ function initMap() {
     document.querySelector('.stores-list').innerHTML=storesHtml;
 
     }
-  //   stores.forEach((store,index)=>{
-  //     console.log(store,index)
-  //     var address=store.addressLines;
-  //     storesHtml+=` <div class="store-container">
-  //     <div class="store-info-container">
-  //     <div class="store-address">
-  //   "addressLines": ["257 S. La Cienega Blvd.", "Beverly Hills, CA 90211"],
-  //   <span>${address[0]}span>
-  //      <span>${address[1]}</span>
-       
-  //     </div>
-  //      <div class="store-phone-number">
-  //      <span> ${store.phoneNumber}</span>
-     
-  //    </div>
-  //  </div>
-  //    <div class="store-number-container">
-  //      <div class="store-number">1</div>
-  //    </div>
-  //    </div>`
-  //   })
+  
 
-  //   document.querySelector('.stores-list').innerHTML=storesHtml;
+  function showStoresMarker(){
 
+    var bounds = new google.maps.LatLngBounds();
+    stores.forEach((store,index)=>{
+      const name=store.name;
+      const address=store.addressLines[0];
+      const latlng = new google.maps.LatLng(
+        store.coordinates.latitude,
+        store.coordinates.longitude)
 
-  // }
+         bounds.extend(latlng);
+        createMarker(latlng,name,address,index+1)
+    });
+    map.fitBounds(bounds)
+
+  }
 
   
+    function createMarker(latlng, name, address,index) {
+      var html = `${name} <br/> + ${address}`;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+        label:String(index)
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+      });
+      markers.push(marker);
+    }
+  
+
+
+    
+
+
+  
+
